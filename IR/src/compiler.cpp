8c8339fc9dfde4c9700ae15fb49cf733272ba5d8
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2017-05-15 14:45:46 -0500

    remove spaghetti from wall

--- IR/src/compiler.cpp
+++ IR/src/compiler.cpp
@@ -213,7 +213,7 @@ void Compiler::Compile(IR::Program p) {
           // store v0 <- dim
           for (int i = 0; i < alloc->dimensions.size(); i++) {
             output << v0 << " <- " << alloc->lhs.name << " + " << (i + alloc->dimensions.size()) * 8 << endl;
-            output << "store " << v0 << " <- " << encode(stoi(alloc->dimensions[i].name)) << endl;
+            output << "store " << v0 << " <- " << alloc->dimensions[i].name << endl;
           }
         }
         else if (shared_ptr<IR::IndexWrite> write = dynamic_pointer_cast<IR::IndexWrite>(i))
