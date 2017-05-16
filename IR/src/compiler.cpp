77a559142a5767d84af4ae904ef69d336679aebf
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2017-05-16 10:03:28 -0500

    Change allocate dim store

--- IR/src/compiler.cpp
+++ IR/src/compiler.cpp
@@ -240,8 +240,8 @@ void Compiler::Compile(IR::Program p) {
           // for each dim:
           // v0 <- v0 + 8
           // store v0 <- dim
-          for (int i = 0; i < alloc->dimensions.size(); i++) {
-            output << v0 << " <- " << alloc->lhs.name << " + " << (1 + i + alloc->dimensions.size()) * 8 << endl;
+          for (int i = 1; i < alloc->dimensions.size(); i++) {
+            output << v0 << " <- " << alloc->lhs.name << " + " << (i * 8) + alloc->dimensions.size() << endl;
             output << "store " << v0 << " <- " << alloc->dimensions[i].name << endl;
           }
         }
